24faab49cc342f99480472bca01a5bd1bb2c8887
Felix Zhang <fe1ix@meta.com>
2024-07-24 13:22:40 -0700

    analog button component
    Summary:
    this diff implements a analog button UI component that has:
    - press and touch ui elements
    - listen for mapped key press events
    - slider controller for the analog input value
    
    Reviewed By: cabanier
    
    Differential Revision:
    D60083864
    
    Privacy Context Container: L1233623
    
    fbshipit-source-id: 8fb1a09218063820dff538400e18a86b50a49a2f

---
+++ devui/src/components/analog.tsx
@@ -0,0 +1,160 @@
+import {
+	Button,
+	ButtonContainer,
+	ButtonGroup,
+	FAIcon,
+	MappedKeyBlock,
+	RangeSelector,
+} from './styled.js';
+import React, { useEffect, useState } from 'react';
+
+import { GamepadIcon } from './icons.js';
+import { MappedKeyDisplay } from './keys.js';
+import { XRController } from '../../../lib/device/XRController';
+import { faFingerprint } from '@fortawesome/free-solid-svg-icons';
+
+interface AnalogButtonProps {
+	xrController: XRController;
+	buttonId: string;
+	pointerLocked: boolean;
+	mappedKey: string;
+}
+
+export const AnalogButton: React.FC<AnalogButtonProps> = ({
+	xrController,
+	buttonId,
+	pointerLocked,
+	mappedKey,
+}) => {
+	const [isTouched, setIsTouched] = useState(false);
+	const [isPressed, setIsPressed] = useState(false);
+	const [isKeyPressed, setIsKeyPressed] = useState(false);
+	const [analogValue, setAnalogValue] = useState(0);
+
+	const handedness = xrController.inputSource.handedness;
+
+	useEffect(() => {
+		const handleKeyDown = (event: KeyboardEvent) => {
+			if (event.code === mappedKey) {
+				xrController.updateButtonValue(buttonId, 1);
+				setIsKeyPressed(true);
+			}
+		};
+
+		const handleKeyUp = (event: KeyboardEvent) => {
+			if (event.code === mappedKey) {
+				xrController.updateButtonValue(buttonId, 0);
+				setIsKeyPressed(false);
+			}
+		};
+
+		const handleMouseDown = (event: MouseEvent) => {
+			if (mappedKey === 'MouseLeft' && event.button === 0) {
+				xrController.updateButtonValue(buttonId, 1);
+				setIsKeyPressed(true);
+			} else if (mappedKey === 'MouseRight' && event.button === 2) {
+				xrController.updateButtonValue(buttonId, 1);
+				setIsKeyPressed(true);
+			}
+		};
+
+		const handleMouseUp = (event: MouseEvent) => {
+			if (mappedKey === 'MouseLeft' && event.button === 0) {
+				xrController.updateButtonValue(buttonId, 0);
+				setIsKeyPressed(false);
+			} else if (mappedKey === 'MouseRight' && event.button === 2) {
+				xrController.updateButtonValue(buttonId, 0);
+				setIsKeyPressed(false);
+			}
+		};
+
+		if (pointerLocked) {
+			if (mappedKey === 'MouseLeft' || mappedKey === 'MouseRight') {
+				window.addEventListener('mousedown', handleMouseDown);
+				window.addEventListener('mouseup', handleMouseUp);
+			} else {
+				window.addEventListener('keydown', handleKeyDown);
+				window.addEventListener('keyup', handleKeyUp);
+			}
+		} else {
+			if (mappedKey === 'MouseLeft' || mappedKey === 'MouseRight') {
+				window.removeEventListener('mousedown', handleMouseDown);
+				window.removeEventListener('mouseup', handleMouseUp);
+			} else {
+				window.removeEventListener('keydown', handleKeyDown);
+				window.removeEventListener('keyup', handleKeyUp);
+			}
+		}
+
+		return () => {
+			if (mappedKey === 'MouseLeft' || mappedKey === 'MouseRight') {
+				window.removeEventListener('mousedown', handleMouseDown);
+				window.removeEventListener('mouseup', handleMouseUp);
+			} else {
+				window.removeEventListener('keydown', handleKeyDown);
+				window.removeEventListener('keyup', handleKeyUp);
+			}
+		};
+	}, [mappedKey, pointerLocked, buttonId, xrController]);
+
+	return (
+		<ButtonContainer $reverse={handedness === 'right'}>
+			<GamepadIcon buttonName={buttonId} handedness={handedness} />
+			<ButtonGroup $reverse={handedness === 'right'}>
+				{pointerLocked ? (
+					<MappedKeyBlock $pressed={isKeyPressed}>
+						{MappedKeyDisplay[mappedKey]}
+					</MappedKeyBlock>
+				) : (
+					<>
+						<Button
+							$reverse={handedness === 'right'}
+							style={{
+								backgroundColor: isPressed
+									? 'rgba(255, 255, 255, 0.6)'
+									: 'rgba(255, 255, 255, 0.3)',
+								width: '50px',
+							}}
+							onClick={() => {
+								setIsPressed(true);
+								xrController.updateButtonValue(buttonId, 1);
+								setTimeout(() => {
+									setIsPressed(false);
+									xrController.updateButtonValue(buttonId, 0);
+								}, 500);
+							}}
+						>
+							Press
+						</Button>
+						<Button
+							$reverse={handedness === 'right'}
+							style={{
+								backgroundColor: isTouched
+									? 'rgba(255, 255, 255, 0.6)'
+									: 'rgba(255, 255, 255, 0.3)',
+								width: '29px',
+							}}
+							onClick={() => {
+								setIsTouched(!isTouched);
+								xrController.updateButtonTouch(buttonId, !isTouched);
+							}}
+						>
+							<FAIcon icon={faFingerprint} />
+						</Button>
+						<RangeSelector
+							$reverse={handedness === 'right'}
+							value={analogValue}
+							onChange={(e) => {
+								const value = Number(e.target.value);
+								setAnalogValue(value);
+								xrController.updateButtonValue(buttonId, value / 100);
+							}}
+							min="0"
+							max="100"
+						/>
+					</>
+				)}
+			</ButtonGroup>
+		</ButtonContainer>
+	);
+};
